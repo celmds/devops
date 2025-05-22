03e91755a09bb3c7c2adf3590d1a06b3a8c0af9b
celmds <celine.bernardinomendes@epfedu.fr>
2025-05-22 11:22:00 +0200

    De

--- TP1/tp-postgres/simpleapi/src/main/java/fr/takima/training/simpleapi/controller/StudentController.java
+++ TP1/tp-postgres/simpleapi/src/main/java/fr/takima/training/simpleapi/controller/StudentController.java
@@ -1,14 +1,23 @@
 package fr.takima.training.simpleapi.controller;
 
-import fr.takima.training.simpleapi.entity.Student;
-import fr.takima.training.simpleapi.service.StudentService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
 import java.net.URI;
 import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import fr.takima.training.simpleapi.entity.Student;
+import fr.takima.training.simpleapi.service.StudentService;
 
 @RestController
 @CrossOrigin
@@ -72,7 +81,7 @@ public class StudentController {
         return ResponseEntity.ok().build();
     }
     @GetMapping("/test")
-    public ResponseEntity<String> test() {
-        return ResponseEntity.ok("TEST OK !!!");
+    public ResponseEntity<Object> test() {
+        return ResponseEntity.ok("Test ok");
     }
 }
